f5730dae6a9210a01ae4b0d8e1553d60db27de61
abbasweb <42547048+abbasweb@users.noreply.github.com>
2019-04-21 19:11:11 +0300

    Update db.php

--- db.php
+++ db.php
@@ -17,7 +17,7 @@ function url2()
 {
    return sprintf
    (
-       "%://%s%s",
+       "%s://%s",
        isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
    $_SERVER['SERVER_NAME']
    );
